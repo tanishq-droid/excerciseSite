f92a55ce0b11d246fbaa3946e8db6859a7aa3273
your name here <tanishqjindal91@gmail.com>
2022-08-11 16:35:57 +0530

    removing warnings

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Typography, Box, Stack } from '@mui/material';
-import Loader from './Loader';
+
 
 const ExerciseVideos = ({ exerciseVideos, name }) => {
   if (!exerciseVideos.length) return `loading...`;
